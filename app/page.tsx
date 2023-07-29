cd408cd0895ac13108279f879a9e6bb8a1a92bb6
anwari-fikri <anwari.fikri@gmail.com>
2023-07-29 10:26:18 +0800

    user authenticate @ home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
+"use client";
+
+import React, { useEffect, useState } from "react";
+import supabase from "./utils/supabaseClient";
+
+const Home = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const [userId, setUserId] = useState<string | undefined>();
+
+    useEffect(() => {
+        const getUser = async () => {
+            const user = await supabase.auth.getUser();
+            console.log("user", user);
+
+            if (user) {
+                const userId = user.data.user?.id;
+                setIsAuthenticated(true);
+                setUserId(userId);
+            }
+        };
+
+        getUser();
+    }, []);
+
     return <div>Home</div>;
-}
+};
+
+export default Home;
