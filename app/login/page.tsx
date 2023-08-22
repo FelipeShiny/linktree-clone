142da29e19e7103cf569c87d38222285a4719f5e
anwari-fikri <anwari.fikri@gmail.com>
2023-08-22 14:36:22 +0800

    login / signup ui update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import supabase from "../utils/supabaseClient";
 import { useRouter } from "next/navigation";
 import { observer } from "mobx-react";
 import AuthStore from "../interfaces/AuthStore";
@@ -28,52 +27,60 @@ const Login = observer(() => {
     }
 
     return (
-        <div>
-            <h1>Login</h1>
-            <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-            >
-                Email
-            </label>
-            <div className="mt-1">
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
-                    placeholder="you@example.com"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </div>
-            <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-            >
-                Password
-            </label>
-            <div className="mt-1">
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
-                    placeholder="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            <button
-                type="button"
-                className="inline-flex items-center rounded-md border border-transparent bg-black text-white"
-                onClick={signInWithEmail}
-            >
-                Login
-            </button>
-            <div>
-                <h2>Don't have account yet a?</h2>
-                <Link href={"/signup"}>
-                    <button>Sign Up</button>
-                </Link>
+        <div className="flex flex-col h-screen items-center justify-center ">
+            <div className="flex flex-col gap-3 border-black rounded-xl p-10 shadow-2xl">
+                <h1 className="text-2xl">Login</h1>
+                <div className="flex flex-col items-left">
+                    <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-700"
+                    >
+                        Email
+                    </label>
+                    <div className="mt-1">
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
+                            placeholder="you@example.com"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col items-left">
+                    <label
+                        htmlFor="password"
+                        className="block text-sm font-medium text-gray-700"
+                    >
+                        Password
+                    </label>
+                    <div className="mt-1">
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
+                            placeholder="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <button
+                    type="button"
+                    className="bg-black text-white px-4 py-1 rounded-lg border-2 hover:bg-white hover:text-black hover:border-black"
+                    onClick={signInWithEmail}
+                >
+                    Login
+                </button>
+                <div className="flex items-center gap-3">
+                    <h2>Need account?</h2>
+                    <Link href={"/signup"}>
+                        <button className="border-2 border-black bg-white text-black px-4 py-1 rounded-lg hover:bg-black hover:text-white">
+                            Sign Up
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,6 +5,7 @@ import supabase from "../utils/supabaseClient";
 import { observer } from "mobx-react";
 import AuthStore from "../interfaces/AuthStore";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const SignUp = observer(() => {
     const router = useRouter();
@@ -26,62 +27,79 @@ const SignUp = observer(() => {
     }
 
     return (
-        <div>
-            <label
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-700"
-            >
-                Username
-            </label>
-            <div className="mt-1">
-                <input
-                    type="text"
-                    name="username"
-                    id="username"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
-                    placeholder="username"
-                    onChange={(e) => setUsername(e.target.value)}
-                />
+        <div className="flex flex-col h-screen items-center justify-center ">
+            <div className="flex flex-col gap-3 border-black rounded-xl p-10 shadow-2xl">
+                <h1 className="text-2xl">Sign Up</h1>
+                <div className="flex flex-col items-left">
+                    <label
+                        htmlFor="username"
+                        className="block text-sm font-medium text-gray-700"
+                    >
+                        Username
+                    </label>
+                    <div className="mt-1">
+                        <input
+                            type="text"
+                            name="username"
+                            id="username"
+                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
+                            placeholder="username"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col items-left">
+                    <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-700"
+                    >
+                        Email
+                    </label>
+                    <div className="mt-1">
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
+                            placeholder="you@example.com"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col items-left">
+                    <label
+                        htmlFor="password"
+                        className="block text-sm font-medium text-gray-700"
+                    >
+                        Password
+                    </label>
+                    <div className="mt-1">
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
+                            placeholder="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <button
+                    type="button"
+                    className="mt-4 bg-black text-white px-4 py-1 rounded-lg border-2 hover:bg-white hover:text-black hover:border-black"
+                    onClick={signUp}
+                >
+                    Sign Up
+                </button>
+                <div className="flex items-center gap-3">
+                    <h2>Already have account kah?</h2>
+                    <Link href={"/login"}>
+                        <button className="border-2 border-black bg-white text-black px-4 py-1 rounded-lg hover:bg-black hover:text-white">
+                            Login
+                        </button>
+                    </Link>
+                </div>
             </div>
-            <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-            >
-                Email
-            </label>
-            <div className="mt-1">
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
-                    placeholder="you@example.com"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </div>
-            <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-            >
-                Password
-            </label>
-            <div className="mt-1">
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
-                    placeholder="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            <button
-                type="button"
-                className="inline-flex items-center rounded-md border border-transparent"
-                onClick={signUp}
-            >
-                Sign Up
-            </button>
         </div>
     );
 });
