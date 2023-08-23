5f4b03fb29707cdc57adaba8f5443c290650d107
anwari-fikri <anwari.fikri@gmail.com>
2023-08-23 10:41:31 +0800

    check for already taken username

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import supabase from "../utils/supabaseClient";
 import { observer } from "mobx-react";
 import AuthStore from "../interfaces/AuthStore";
@@ -14,21 +14,64 @@ const SignUp = observer(() => {
     const [password, setPassword] = useState<string | undefined>();
     const [username, setUsername] = useState<string | undefined>();
 
-    async function signUp() {
-        try {
-            if (email && password && username) {
-                await AuthStore.signUpWithEmail(email, password, username);
-                console.log("Sign up successful");
-                router.push(`/${username}`);
+    async function checkUsernameTaken() {
+        if (username) {
+            try {
+                console.log("Checking username availability for:", username);
+                const { data, error } = await supabase
+                    .from("users")
+                    .select("username")
+                    .eq("username", username)
+                    .maybeSingle();
+
+                if (error) {
+                    console.error("Error:", error);
+                    return false;
+                }
+
+                if (data) {
+                    console.log("Username already taken:", data);
+                    return true;
+                }
+
+                return false;
+            } catch (error) {
+                console.error("Error checking username:", error);
+                return false;
             }
-        } catch (error) {
-            console.log("Signup error:", error);
         }
     }
 
+    async function signUp(e: any) {
+        e.preventDefault();
+
+        const isUsernameTaken = await checkUsernameTaken();
+
+        if (!isUsernameTaken) {
+            console.log("You can signup!");
+            try {
+                if (email && password && username) {
+                    await AuthStore.signUpWithEmail(email, password, username);
+                    console.log("Sign up successful");
+                    router.push(`/${username}`);
+                }
+            } catch (error) {
+                console.log("Signup error:", error);
+            }
+            // Proceed with signup logic here
+        } else {
+            console.log("username is taken. choose another one");
+        }
+    }
+
+    // async function signUp() {
+    //     await setUsernameTaken(checkUsernameTaken());
+
+    // }
+
     return (
         <div className="flex flex-col h-screen items-center justify-center ">
-            <div className="flex flex-col gap-3 border-black rounded-xl p-10 shadow-2xl">
+            <form className="flex flex-col gap-3 border-black rounded-xl p-10 shadow-2xl">
                 <h1 className="text-2xl">Sign Up</h1>
                 <div className="flex flex-col items-left">
                     <label
@@ -45,6 +88,7 @@ const SignUp = observer(() => {
                             className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
                             placeholder="username"
                             onChange={(e) => setUsername(e.target.value)}
+                            required
                         />
                     </div>
                 </div>
@@ -63,6 +107,7 @@ const SignUp = observer(() => {
                             className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
                             placeholder="you@example.com"
                             onChange={(e) => setEmail(e.target.value)}
+                            required
                         />
                     </div>
                 </div>
@@ -81,25 +126,25 @@ const SignUp = observer(() => {
                             className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 "
                             placeholder="password"
                             onChange={(e) => setPassword(e.target.value)}
+                            required
                         />
                     </div>
                 </div>
                 <button
-                    type="button"
                     className="mt-4 bg-black text-white px-4 py-1 rounded-lg border-2 hover:bg-white hover:text-black hover:border-black"
                     onClick={signUp}
                 >
                     Sign Up
                 </button>
                 <div className="flex items-center gap-3">
-                    <h2>Already have account kah?</h2>
+                    <h2>Already have an account?</h2>
                     <Link href={"/login"}>
                         <button className="border-2 border-black bg-white text-black px-4 py-1 rounded-lg hover:bg-black hover:text-white">
                             Login
                         </button>
                     </Link>
                 </div>
-            </div>
+            </form>
         </div>
     );
 });
