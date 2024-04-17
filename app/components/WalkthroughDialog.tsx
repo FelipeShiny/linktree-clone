5412c3a9d5115bf91c523c30bb70fc1bd9a6f7ed
anwari-fikri <anwari.fikri@gmail.com>
2024-04-17 10:36:13 +0800

    update walkthrough text

--- app/components/WalkthroughDialog.tsx
+++ app/components/WalkthroughDialog.tsx
@@ -39,36 +39,36 @@ export function WalkthroughDialog() {
 
                     <p className="font-bold">2. Log in as creator</p>
                     <h3 className="pb-2 text-base">Login</h3>
-                    <DefaultCheckbox instruction="Click 'Login' button" />
+                    <DefaultCheckbox instruction="Click 'Login' button - located at the top right" />
                     <DefaultCheckbox instruction="Use the provided login credential" />
                     <h3 className="pb-2 text-base ">Add new link</h3>
-                    <DefaultCheckbox instruction="Go to test_user creator page" />
-                    <DefaultCheckbox instruction="Enter the title and the URL of your new link then press 'Add new link' using the form at the bottom of the page" />
+                    <DefaultCheckbox instruction="Go to test_user admin page - it's at the top right profile button" />
+                    <DefaultCheckbox instruction="Enter the URL of your new link" />
                     <DefaultCheckbox instruction="Try your newly added link" />
+                    <h3 className="pb-2 text-base ">Edit link</h3>
+                    <DefaultCheckbox instruction="Click the pencil icon on your link" />
+                    <DefaultCheckbox instruction="Edit your link" />
+                    <DefaultCheckbox instruction="Click the tick button when you are done" />
                     <h3 className="pb-2 text-base ">Delete link</h3>
-                    <DefaultCheckbox instruction="On the right side of the link, press the '⋮' button then press delete" />
-                    <h3 className="pb-2 text-base ">Authentication</h3>
-                    <DefaultCheckbox instruction="Go back to the home page and choose other creator page" />
-                    <DefaultCheckbox instruction="Notice that you cannot add link for other creator other than yours" />
+                    <DefaultCheckbox instruction="On the right side of the link, press the trashcan icon to delete" />
                     <h3 className="pb-2 text-base ">Change profile picture</h3>
-                    <DefaultCheckbox instruction="Go to test_user creator page" />
-                    <DefaultCheckbox instruction="Under 'Upload Profile Picture', press 'Choose File'. Select any image you want to use for profile picture" />
-                    <DefaultCheckbox instruction="The page should refresh itself and you will see your new profile picture" />
+                    <DefaultCheckbox instruction="After clicking the pencil button on your profile picture, press 'Choose File'" />
+                    <DefaultCheckbox instruction="Select any image you want to use for profile picture then upload. You will see your new profile picture" />
 
                     <p className="font-bold">3. Sign up as creator</p>
                     <h3 className="pb-2 text-base ">Sign Up</h3>
                     <DefaultCheckbox instruction="Click 'Login' button" />
                     <DefaultCheckbox instruction="On Login page, Click 'Sign Up' button" />
                     <DefaultCheckbox instruction="Enter your username, email and password" />
-                    <DefaultCheckbox instruction="You should be redirected to your creator page and be able to do all the things from previous tutorial" />
+                    <DefaultCheckbox instruction="You should be redirected to your admin page and be able to do all the things from previous tutorial" />
 
-                    <div className="relative mt-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700">
+                    {/* <div className="relative mt-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700">
                         <strong className="font-bold">Warning!</strong>
                         <p>
                             Please use throwaway email and password because I do
                             not even know how secure my app is lol
                         </p>
-                    </div>
+                    </div> */}
                 </DialogHeader>
             </DialogContent>
         </Dialog>
